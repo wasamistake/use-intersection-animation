2fe76bcf2f9a0d2160c26347e8193743654f7989
wasamistake <150041252+wasamistake@users.noreply.github.com>
2023-11-29 14:46:54 -0300

    Add animation: `rotateRight`

--- src/animations.ts
+++ src/animations.ts
@@ -26,6 +26,19 @@ export const rotateLeft: SyntheticEffect = {
   },
 }
 
+export const rotateRight: SyntheticEffect = {
+  keyframes: {
+    opacity: [0, 1],
+    transform: ['rotate3d(0, 0, 1, -180deg)', 'rotate3d(0, 0, 0, 0deg)'],
+  },
+  options: {
+    duration: 500,
+    easing: 'ease',
+    fill: 'both',
+    id: 'rotate-right',
+  },
+}
+
 export const scale: SyntheticEffect = {
   keyframes: {
     opacity: [0, 1],

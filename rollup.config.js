ce32294b5fe9aadacf76e5f5b3a0a1a627eb350b
wasamistake <150041252+wasamistake@users.noreply.github.com>
2024-02-20 14:59:10 -0300

    Remove unused `react-dom` dependency

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default defineConfig([
         sourcemap: true,
       },
     ],
-    external: ['react', 'react-dom'],
+    external: ['react'],
     plugins: [nodeResolve(), typescript({ exclude }), terser()],
   },
   {
